152bde6d2aeaf3275a6df69b0e9f42e4b38ce0d9
justnoserena <justnoserena@gmail.com>
2022-07-21 21:27:38 -0700

    Updated README

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+const questOne = [{
+    number: one,
+    question:"What does HTML stand for?",
+    options: ["Hotmail","HyperText Markup Language","Home Tool Markup Language","Hyperlinks and Text Markup Language"],
+    correct:"HyperText Markup Language",
+},
+{
+    number: two,
+    question:"What is an object?",
+    options: ["A type of variable","A type of array","A type of function","All of the above"],
+    correct:"All of the above",
+},
+{
+    number: three,
+    question:"quest 3",
+    options: ["1w","2w","3w"],
+    correct: "1c",
+},
+{
+    number: four,
+    question:"quest 4",
+    options: ["1w","2w","3w"],
+    correct: "1c",
+},
+{
+    number: five,
+    question:"quest 5",
+    options: ["1w","2w","3w"],
+    correct: "1c",
+}]
+
+const questGen = document.getElementsByClassName("option");
+
+
